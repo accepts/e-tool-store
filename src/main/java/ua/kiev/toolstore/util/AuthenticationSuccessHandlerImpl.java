b87baafbdabb4045773a26dada7dfeaf5c761972
accepts <accepts@ya.ru>
2017-03-14 14:43:16 +0200

    Day#33: Start working with Angular

--- src/main/java/ua/kiev/toolstore/util/AuthenticationSuccessHandlerImpl.java
+++ src/main/java/ua/kiev/toolstore/util/AuthenticationSuccessHandlerImpl.java
@@ -1,27 +1,21 @@
 package ua.kiev.toolstore.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Service;
-import ua.kiev.toolstore.util.validator.UserUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @Service
 public class AuthenticationSuccessHandlerImpl implements ApplicationListener<AuthenticationSuccessEvent> {
 
     protected static final LoggerWrapper LOG = LoggerWrapper.get(AuthenticationSuccessHandlerImpl.class);
 
-    @Autowired
-    private UserUtil userUtil;
-
-
     @Override
     public void onApplicationEvent(AuthenticationSuccessEvent authenticationSuccessEvent) {
-        LOG.debug("<---login success-");
-//        userUtil.setItemsInOrder(6);
-//        LOG.debug("<----LOGIN Success for userID: ( " + userUtil.getUserId()
-//                + " ), userEmail: (" + userUtil.getEmail() + " ) \n items in Order: ( " + userUtil.getItemsInOrder()
-//                + " )"  );
+        LOG.debug("<---login success: "
+                + new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime()));
     }
 
 
